382537599fcb9d9513176d594d315727186028ec
Clément Chameyrat <clement.chameyrat@voluntis.com>
2023-06-05 10:49:43 +0200

    Update version

--- app/src/main/java/com/elyeproj/interpolatorplotter/PlotterActivity.kt
+++ app/src/main/java/com/elyeproj/interpolatorplotter/PlotterActivity.kt
@@ -11,18 +11,22 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.interpolator.view.animation.LinearOutSlowInInterpolator
 import com.elyeproj.interpolatorplotter.PlotterView.Companion.DURATION
 import com.elyeproj.interpolatorplotter.PlotterView.Companion.MAX
-import kotlinx.android.synthetic.main.activity_plotter.*
+import com.elyeproj.interpolatorplotter.databinding.ActivityPlotterBinding
 
 
 class PlotterActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityPlotterBinding
+
     private val buttonHeight by lazy {
-        button_fly.height / 2
+        binding.buttonFly.height / 2
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_plotter)
+        binding = ActivityPlotterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
 
         ArrayAdapter.createFromResource(
             this,
@@ -32,10 +36,10 @@ class PlotterActivity : AppCompatActivity() {
             // Specify the layout to use when the list of choices appears
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             // Apply the adapter to the spinner
-            interpolator_spinner.adapter = adapter
+            binding.interpolatorSpinner.adapter = adapter
         }
 
-        btn_plotter.setOnClickListener {
+        binding.btnPlotter.setOnClickListener {
             startPlotting()
         }
     }
@@ -43,38 +47,47 @@ class PlotterActivity : AppCompatActivity() {
     private fun startPlotting() {
         val (selectedInterpolator, range) = getInterpolator()
 
-        view_plotter.setHeightRange(range.first, range.second)
+        binding.viewPlotter.setHeightRange(range.first, range.second)
         ValueAnimator.ofFloat(0f, MAX).apply {
             interpolator = selectedInterpolator
             duration = DURATION.toLong()
             addUpdateListener { animation ->
                 val progress = animation.animatedValue as Float
-                val buttonY = view_plotter.drawPath(animation.currentPlayTime.toFloat(), progress)
+                val buttonY = binding.viewPlotter.drawPath(animation.currentPlayTime.toFloat(), progress)
                 buttonY?.let {
-                    button_fly.y = it - buttonHeight
+                    binding.buttonFly.y = it - buttonHeight
                 }
             }
             addListener(object : AnimatorListenerAdapter() {
-                override fun onAnimationStart(animation: Animator?) {
-                    btn_plotter.isEnabled = false
+                override fun onAnimationStart(animation: Animator) {
+                    binding.btnPlotter.isEnabled = false
                 }
+
                 override fun onAnimationEnd(animation: Animator) {
-                    btn_plotter.isEnabled = true
+                    binding.btnPlotter.isEnabled = true
                 }
             })
         }.start()
     }
 
     private fun getInterpolator(): Pair<Interpolator, Pair<Float, Float>> {
-        return when (interpolator_spinner.selectedItem) {
+        return when (binding.interpolatorSpinner.selectedItem) {
             "LinearInterpolator" -> Pair(LinearInterpolator(), Pair(0f, 1f))
             "LinearOutSlowInInterpolator" -> Pair(LinearOutSlowInInterpolator(), Pair(0f, 1f))
             "AccelerateInterpolator" -> Pair(AccelerateInterpolator(), Pair(0f, 1f))
             "DecelerateInterpolator" -> Pair(DecelerateInterpolator(), Pair(0f, 1f))
-            "AccelerateDecelerateInterpolator" -> Pair(AccelerateDecelerateInterpolator(), Pair(0f, 1f))
+            "AccelerateDecelerateInterpolator" -> Pair(
+                AccelerateDecelerateInterpolator(),
+                Pair(0f, 1f)
+            )
+
             "OvershootInterpolator" -> Pair(OvershootInterpolator(), Pair(0f, 1.2f))
             "AnticipateInterpolator" -> Pair(AnticipateInterpolator(), Pair(-0.2f, 1f))
-            "AnticipateOvershootInterpolator" -> Pair(AnticipateOvershootInterpolator(), Pair(-0.2f, 1.2f))
+            "AnticipateOvershootInterpolator" -> Pair(
+                AnticipateOvershootInterpolator(),
+                Pair(-0.2f, 1.2f)
+            )
+
             "CycleInterpolator" -> Pair(CycleInterpolator(2f), Pair(-1f, 1f))
             "BounceInterpolator" -> Pair(BounceInterpolator(), Pair(0f, 1f))
             "PathInterpolator" -> {
@@ -91,6 +104,7 @@ class PlotterActivity : AppCompatActivity() {
                 path.lineTo(1f, 1f)
                 Pair(PathInterpolator(path), Pair(0f, 1f))
             }
+
             "HesitateInterpolator" -> Pair(HesitateInterpolator(), Pair(0f, 1f))
             "SpringInterpolator" -> Pair(SpringInterpolator(), Pair(0f, 1.4f))
             "CircularSpringInterpolator" -> Pair(CircularSpringInterpolator(), Pair(-1f, 1f))
